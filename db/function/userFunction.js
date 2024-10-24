b9aa34e154a806e7ad1ba372969b56e6d34a0696
ananyanivarthi <ananyanivarthi@gmail.com>
2024-10-24 09:53:57 +0530

    userFunction

--- db/function/userFunction.js
+++ db/function/userFunction.js
@@ -1,15 +1,15 @@
 const bcrypt = require('bcryptjs');
 
 // Create user function using email
-async function createUser(usersCollection, email, password) {
+async function createUser(usersCollection, username, password) {
   const hashedPassword = await bcrypt.hash(password, 10);
-  const user = { email, password: hashedPassword }; // Store email instead of username
+  const user = { username, password: hashedPassword }; // store username
   await usersCollection.insertOne(user);
 }
 
 // Find user by email
 async function findUser(usersCollection, username) {
-  return await usersCollection.findOne({ username }); // Search by email
+  return await usersCollection.findOne({ username }); // Search by username
 }
 
 // Compare passwords
